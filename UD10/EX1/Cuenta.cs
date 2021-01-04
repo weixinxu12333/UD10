e20729e56be4527b3e31cc4b561062090135edab
Weixin <weixin.xu.2018@gmail.com>
2021-01-04 22:42:08 +0100

    EX1 ACABADO

---
+++ UD10/EX1/Cuenta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10
+{
+    class Cuenta
+    {
+        public string titular { get; set; }
+        public double cantidad { get; set; }
+
+        public Cuenta(string titular)
+        {
+            this.titular = titular;
+        }
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public override string ToString() {
+            return titular + " " + cantidad;
+        }
+
+        public void Ingresar(double cantidad)
+        {
+            if (cantidad > 0)
+                this.cantidad = cantidad;
+        }
+
+        public void Retirar(double cantidad)
+        {
+            if (this.cantidad - cantidad <= 0)
+                this.cantidad = 0;
+            else
+                this.cantidad = this.cantidad - cantidad;
+
+        }
+    }
+}
