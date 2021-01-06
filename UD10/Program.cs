26b38ebff16d0bf338957f63c38180e7828879fe
Weixin <weixin.xu.2018@gmail.com>
2021-01-06 18:01:56 +0100

    EX3 ACABADO

--- UD10/Program.cs
+++ UD10/Program.cs
@@ -23,6 +23,22 @@ namespace UD10
                 .Add("Ex2", () => {
                     PruebaPersona.Inicio();
                 })
+                .Add("Ex3", () => {
+                    Console.WriteLine("Introduce tamaño de un array: ");
+                    int cantidad = Convert.ToInt32(Console.ReadLine());
+                    Password[] arrayPassword = new Password[cantidad];
+                    Console.WriteLine("Introduce longitud de los passwords: ");
+                    int lon = Convert.ToInt32(Console.ReadLine());
+
+                    bool[] arraySeguro = new bool[cantidad];
+
+                    for (int i = 0; i < arrayPassword.Length; i++)
+                    {
+                        arrayPassword[i] = new Password(lon);
+                        arraySeguro[i] = arrayPassword[i].esFuerte();
+                        Console.WriteLine("{0} {1}", arrayPassword[i].contraseña, arraySeguro[i]);
+                    }
+                })
                 .Add("Salir", () => salir = true);
             while (!salir)
                 menu.Display();
